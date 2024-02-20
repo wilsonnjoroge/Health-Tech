3cc7cd88f11eb5f3ab6e89b4ad7b04ab9254cf40
Wilsonnjoroge <wilsonnjoroge932@gmail.com>
2024-02-20 15:39:18 +0300

    Deleted the database uri. Will be replaced with the actual DB link

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use("/api", router);
 
-mongoose.connect("mongodb+srv://wilsonnjoroge932:whyalwaysme@patientsregdata.ygcy4sk.mongodb.net/patientdata?retryWrites=true&w=majority"
+mongoose.connect("<ACTUAL_DATABASE_LINK>"
 ).then(() => app.listen(3600)
 ).then(() => console.log("Connected to Database and Listens from: 3600")).catch((err) => console.log(err));
 
