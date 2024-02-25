a4717820f734150ccaca8ee5f3d48522de93c99e
Wilsonnjoroge <wilsonnjoroge932@gmail.com>
2024-02-25 16:34:47 +0300

    Separated endpoints for specialists and patients

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { getAllUsers, registerNewPatient, logIn, forgotPassword  } from "../Controllers/Controllers.js";
+import { registerNewPatient, logInPatient, forgotPassword, getAllPatients  } from "../Controllers/patient_controllers.js";
+import { getAllSpecialists, logInSpecialist, registerNewSpecialist } from "../Controllers/specialist_controller.js";
 
 const router = express.Router();
 
-router.get("/users/view", getAllUsers);
-router.post("/users/create", registerNewPatient);
-router.post("/auth/login", logIn);
+//Routes for handling patients
+router.get("/users/viewpatients", getAllPatients);
+router.post("/users/register/patient", registerNewPatient);
+router.post("/auth/login/patient", logInPatient);
+router.post("/auth/forgotPassword", forgotPassword);
+
+//Routes for handling specialists
+router.get("/users/viewspecialist", getAllSpecialists);
+router.post("/users/register/specialist", registerNewSpecialist);
+router.post("/auth/login/specialist", logInSpecialist);
 router.post("/auth/forgotPassword", forgotPassword);
 
 export default router;
