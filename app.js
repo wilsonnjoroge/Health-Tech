3d735a922531aac550624df678c93a5226374919
Wilsonnjoroge <wilsonnjoroge932@gmail.com>
2024-02-25 16:39:36 +0300

    Provided a DB Link in the .env file

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./Routes/Routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 const app = express();
 app.use(express.json());
 app.use("/api", router);
 
-mongoose.connect("<ACTUAL_DATABASE_LINK>"
+mongoose.connect(process.env.DB_URL
 ).then(() => app.listen(3600)
 ).then(() => console.log("Connected to Database and Listens from: 3600")).catch((err) => console.log(err));
 
